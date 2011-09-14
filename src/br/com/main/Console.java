993cece3e437bef3a640d84f6f959cfb8c881c22
febiasi <febiasi@gmail.com>
2011-09-14 01:47:20 +0000

    Testando com modificação no console

--- src/br/com/main/Console.java
+++ src/br/com/main/Console.java
@@ -5,8 +5,12 @@ public class Console {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		System.out.println("****************************************************************");
+		System.out.println("* Bem vindo ao Projeto de Felipe Biasi e Guilherme Campos *");
+		System.out.println("****************************************************************\n");
 
 	}
 
